80fab0504a80614d346d697a02bb6fdd8892d2bd
Jakub Krok <jakub.krok@microsoft.wsei.edu.pl>
2022-03-12 16:03:21 +0100

    main2

--- src/Main2.java
+++ src/Main2.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main2 {
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+
+        byte[] data;
+        try {
+            data = Files.readAllBytes(Paths.get("file.txt"));
+        } catch (IOException e) {
+            throw e;
+        }
+        String text = new String(data, StandardCharsets.UTF_8);
+        System.out.println(text);
+    }
 }
