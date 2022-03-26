5a3ef6b803bc57762539f28459233ad8e40e37bd
Jakub Krok <jakub.krok@microsoft.wsei.edu.pl>
2022-03-26 16:07:05 +0100

    zajecia_2

---
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/template")
+
+    public String template()
+    {
+        return "index";
+    }
+
+    @RequestMapping("/hello{nazwa}")
+    @ResponseBody
+    public String index2(
+            @PathVariable String nazwa,
+            @RequestParam Integer age,
+            @RequestParam Boolean test
+    )
+    {
+        return "Hello " + nazwa + "! age: " + age + " test: " + test;
+    }
+}
