13f42b0c87619a215c823daa8624ecba959d69a8
Jakub Krok <50359069+Brtgkrk@users.noreply.github.com>
2022-06-22 22:34:50 +0200

    Get and create course

--- backend_projekt/controllers/courseController.js
+++ backend_projekt/controllers/courseController.js
@@ -1,43 +1,62 @@
 const Course = require('./../models/courseModel');
 
-exports.getAllCourses = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        // results: courses.length,
-        // data: {
-        //     courses
-        // }
-    });
+exports.getAllCourses = async (req, res) => {
+
+    try {
+        const courses = await Course.find();
+
+        res.status(200).json({
+            status: 'success',
+            results: courses.length,
+            data: {
+                courses
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
+    
 };
 
-exports.getCourse = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id * 1;
-    const course = courses.find(el => el.id === id);
+exports.getCourse = async (req, res) => {
     
-    // if (!course)
-    // {
-    //     return res.status(404).json({
-    //         status: 'fail',
-    //         message: 'Invalid ID'
-    //     });
-    // }
-    
-    // res.status(200).json({
-    //     status: 'success',
-    //     data: {
-    //         course
-    //     }
-    // });
+    try {
+        const course = await Course.findById(req.params.id);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                course
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
 
-exports.createCourse = (req, res) => {
-    res.status(201).json({
-        status: 'success',
-        // data: {
-        //     course: newCourse
-        // }
-    });
+exports.createCourse = async (req, res) => {
+
+    try {
+        const newCourse = await Course.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                course: newCourse
+            }
+        });
+    } catch(err) {
+        res.status(400).json({
+            status: 'fail',
+            message: 'Invalid data sent!'
+        })
+    }    
 };
 
 exports.updateCourse = (req, res) => {
