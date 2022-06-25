79ab4668ba4a69ca253d01b0538ae4582cb6953c
Jakub Krok <50359069+Brtgkrk@users.noreply.github.com>
2022-06-25 15:24:11 +0200

    user register and login work

--- backend_projekt/app.js
+++ backend_projekt/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
 const courseRouter = require('./routes/courseRoutes');
 const userRouter = require('./routes/userRoutes');
--- backend_projekt/controllers/authController.js
+++ backend_projekt/controllers/authController.js
@@ -1,13 +1,14 @@
 const jwt = require('jsonwebtoken');
-const User = require('./../models/userMode');
+const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
 const signToken = id => {
+    console.log(process.env.JWT_SECRET)
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
+      expiresIn: process.env.JWT_EXPIRES_IN
     });
-}
+  };
 
 exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create(req.body);
